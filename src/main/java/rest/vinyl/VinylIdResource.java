d44d64c25db74ee22ad0908dd2fd10f48b2953a5
Dumbyarnik <62794996+Dumbyarnik@users.noreply.github.com>
2022-02-08 20:46:35 +0100

    Made VinylId mature

--- src/main/java/rest/vinyl/VinylIdResource.java
+++ src/main/java/rest/vinyl/VinylIdResource.java
@@ -22,6 +22,12 @@ import javax.ws.rs.core.Response.Status;
 
 import org.eclipse.microprofile.openapi.annotations.Operation;
 
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
 import control.DAO.ClientDAO;
 import control.DAO.ContactDAO;
 import control.DAO.CreateClientDAO;
@@ -43,6 +49,7 @@ import javax.ws.rs.core.MediaType;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class VinylIdResource {
+
     @Inject
     VinylBoundary vinylController = new VinylController();
 
@@ -52,12 +59,33 @@ public class VinylIdResource {
 
     @GET
     @PermitAll
+    @Operation(summary = "Gets the vinyl")
+    @APIResponses(value = {
+        @APIResponse(responseCode = "200", 
+            description = "Success",
+            content = @Content(mediaType = "application/json",
+            schema = @Schema(implementation = VinylDTO.class))),
+        @APIResponse(responseCode = "406", 
+            description = "Vinyl doesn't exist",
+            content = @Content(mediaType = "text/plain"))
+        }
+    )
     public Response getVinyl(@PathParam("id") Long id) {
         return Response.ok(vinylController.getVinyl(id)).build();
     }
 
     @PUT
     @RolesAllowed("Client")
+    @Operation(summary = "Gets the profile")
+    @APIResponses(value = {
+        @APIResponse(responseCode = "200", 
+            description = "Success",
+            content = @Content(mediaType = "application/json")),
+        @APIResponse(responseCode = "406", 
+            description = "Client or vinyl doesn't exist",
+            content = @Content(mediaType = "text/plain"))
+        }
+    )
     public Response updateVinyl(@PathParam("id") Long id,
         VinylDTO vinylDTO) {
         vinylController.updateVinyl(id, vinylDTO);
@@ -66,10 +94,32 @@ public class VinylIdResource {
 
     @DELETE
     @RolesAllowed("Client")
+    @Operation(summary = "Gets the profile")
+    @APIResponses(value = {
+        @APIResponse(responseCode = "200", 
+            description = "Success",
+            content = @Content(mediaType = "application/json")),
+        @APIResponse(responseCode = "406", 
+            description = "Client or vinyl doesn't exist",
+            content = @Content(mediaType = "text/plain"))
+        }
+    )
     public Response deleteVinyl(@PathParam("id") Long id) {
         if (vinylController.deleteVinyl(id))
             return Response.ok().build();
-        return Response.ok("Vinyl doesn't exist").build();
+        return Response.status(406).entity("Client or vinyl doesn't exist").build();
+    }
+
+    //////////////////// NOT AVAILABLE ///////////////////////////////////
+
+    @POST
+    @PermitAll
+    @Operation(summary = "Doesn't exist")
+    @APIResponses(value = @APIResponse(responseCode = "404", 
+            description = "Not Found",
+            content = @Content(mediaType = "text/plain")))
+    public Response createVinyl() {
+        return Response.status(404).entity("Method doesn't exist").build();
     }
 
 
