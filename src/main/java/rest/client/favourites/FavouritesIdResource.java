053d0e3e2412ee79bd9b6b59b5109f7c98abe34f
Dumbyarnik <62794996+Dumbyarnik@users.noreply.github.com>
2022-02-10 13:07:37 +0100

    Cleaned favourites

---
+++ src/main/java/rest/client/favourites/FavouritesIdResource.java
@@ -0,0 +1,111 @@
+package rest.client.favourites;
+
+import java.security.Principal;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+
+import control.client.favourites.FavouritesBoundary;
+import control.client.favourites.FavouritesController;
+
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+
+import javax.ws.rs.core.Context;
+
+// http://localhost:8080/client/favourites/{vinyl_id}
+@ApplicationScoped
+@Path("/client/favourites/{vinyl_id}")
+public class FavouritesIdResource {
+
+    @Inject
+    FavouritesBoundary favouritesController = new FavouritesController();
+
+    @PostConstruct
+    public void init() {  
+    }
+
+    @POST
+    @RolesAllowed("Client")
+    @Operation(summary = "Creates the favourite")
+    @APIResponses(value = {
+        @APIResponse(responseCode = "200", 
+            description = "Success",
+            content = @Content(mediaType = "application/json")),
+        @APIResponse(responseCode = "406", 
+            description = "Client or Vinyl doesn't exist",
+            content = @Content(mediaType = "text/plain"))
+        }
+    )
+    public Response createFavourite(@Context SecurityContext sec,
+        @PathParam("vinyl_id") Long vinyl_id) {
+        Principal user = sec.getUserPrincipal();
+        String username = user.getName();
+
+        if (favouritesController.createFavourite(username, vinyl_id))
+            return Response.ok().build();
+        
+        return Response.status(406).entity("Vinyl doesn't exist").build(); 
+    }
+
+    @DELETE
+    @RolesAllowed("Client")
+    @Operation(summary = "Deletes the favourite")
+    @APIResponses(value = {
+        @APIResponse(responseCode = "200", 
+            description = "Success"),
+        @APIResponse(responseCode = "406", 
+            description = "Vinyl or client doesn't exist",
+            content = @Content(mediaType = "text/plain"))
+        }
+    )
+    public Response deleteVinyl(@Context SecurityContext sec,
+    @PathParam("vinyl_id") Long vinyl_id) {
+        Principal user = sec.getUserPrincipal();
+        String username = user.getName();
+
+        if (favouritesController.deleteFavourite(username, vinyl_id))
+            return Response.ok().build();
+        return Response.status(406).entity("Vinyl or client doesn't exist").build();
+    }
+
+
+    //////////////////// NOT AVAILABLE ///////////////////////////////////
+
+    @GET
+    @PermitAll
+    @Operation(summary = "Doesn't exist")
+    @APIResponses(value = @APIResponse(responseCode = "404", 
+            description = "Not Found",
+            content = @Content(mediaType = "text/plain")))
+    public Response getFavourite() {
+        return Response.status(404).entity("Method doesn't exist").build();
+    }
+
+    @PUT
+    @PermitAll
+    @Operation(summary = "Doesn't exist")
+    @APIResponses(value = @APIResponse(responseCode = "404", 
+            description = "Not Found",
+            content = @Content(mediaType = "text/plain")))
+    public Response updateFavourite() {
+        return Response.status(404).entity("Method doesn't exist").build();
+    }
+
+    
+    
+}
--- src/main/java/rest/client/favourites/FavouritesResource.java
+++ src/main/java/rest/client/favourites/FavouritesResource.java
@@ -1,4 +1,4 @@
-package rest.favourites;
+package rest.client.favourites;
 
 import java.security.Principal;
 
@@ -7,50 +7,30 @@ import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
-import javax.ws.rs.core.Response.Status;
 
 import org.eclipse.microprofile.openapi.annotations.Operation;
 
-import control.DTO.ClientDTO;
-import control.DTO.ContactDTO;
-import control.DTO.CreateClientDTO;
-import control.DTO.CreateVinylDTO;
 import control.DTO.VinylDTO;
-import control.client.ClientBoundry;
-import control.client.ClientController;
-import control.favourites.FavouritesBoundary;
-import control.favourites.FavouritesController;
-import control.vinyl.VinylBoundary;
-import control.vinyl.VinylController;
-import entities.ClientGateway;
-import entities.basic.Client;
-import gateway.ClientRepository;
+import control.client.favourites.FavouritesBoundary;
+import control.client.favourites.FavouritesController;
 
-import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
 
 // http://localhost:8080/client/favourites
 @ApplicationScoped
 @Path("/client/favourites")
-@Produces(MediaType.APPLICATION_JSON)
-@Consumes(MediaType.APPLICATION_JSON)
 public class FavouritesResource {
 
     @Inject
